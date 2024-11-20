fed8d3cf163e455ef80710ddeb4f9d801339b40f
Mohammad Mohammad <it.msm88@gmail.com>
2024-11-20 18:15:52 +0100

    render an empty webview

--- reacttriggeractions/out/extension.js
+++ reacttriggeractions/out/extension.js
@@ -31,18 +31,30 @@ const vscode = __importStar(require("vscode"));
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 function activate(context) {
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "reacttriggeractions" is now active!');
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with registerCommand
-    // The commandId parameter must match the command field in package.json
-    const disposable = vscode.commands.registerCommand('reacttriggeractions.start', () => {
-        // The code you place here will be executed every time your command is executed
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World from ReactTriggerActions!');
-    });
-    context.subscriptions.push(disposable);
+    context.subscriptions.push(vscode.commands.registerCommand('trigger-actions.openWebview', () => {
+        const panel = vscode.window.createWebviewPanel('triggerActions', 'Trigger Actions', vscode.ViewColumn.One, {
+            enableScripts: true,
+        });
+        //const webviewUri = getUri(context.extensionUri, ['dist', 'index.html']);
+        const webviewUri = vscode.Uri.parse('https://example.com');
+        panel.webview.html = getWebviewContent(webviewUri);
+    }));
+}
+function getWebviewContent(webviewUri) {
+    return `
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Trigger Actions</title>
+        </head>
+        <body>
+            <div id="root"></div>
+            <script type="module" src="${webviewUri}"></script>
+        </body>
+        </html>
+    `;
 }
 // This method is called when your extension is deactivated
 function deactivate() { }
--- reacttriggeractions/src/extension.ts
+++ reacttriggeractions/src/extension.ts
@@ -2,24 +2,45 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "reacttriggeractions" is now active!');
+	context.subscriptions.push(
+        vscode.commands.registerCommand('trigger-actions.openWebview', () => {
+            const panel = vscode.window.createWebviewPanel(
+                'triggerActions',
+                'Trigger Actions',
+                vscode.ViewColumn.One,
+                {
+                    enableScripts: true,
+                }
+            );
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('reacttriggeractions.start', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from ReactTriggerActions!');
-	});
+            //const webviewUri = getUri(context.extensionUri, ['dist', 'index.html']);
+			const webviewUri =  vscode.Uri.parse('https://example.com');
+            panel.webview.html = getWebviewContent(webviewUri);
+        })
+    );
+	
+}
 
-	context.subscriptions.push(disposable);
+function getWebviewContent(webviewUri: vscode.Uri): string {
+    return `
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Trigger Actions</title>
+        </head>
+        <body>
+            <div id="root"></div>
+            <script type="module" src="${webviewUri}"></script>
+        </body>
+        </html>
+    `;
 }
 
 // This method is called when your extension is deactivated
